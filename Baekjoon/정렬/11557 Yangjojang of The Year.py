416ee2aa201aa92cfd1660673ff413db7715e51d
jwo0o0 <jwo0o0358@gmail.com>
2024-10-03 00:53:54 +0900

    주석 추가

--- Baekjoon/정렬/11557 Yangjojang of The Year.py
+++ Baekjoon/정렬/11557 Yangjojang of The Year.py
@@ -3,16 +3,17 @@ read = sys.stdin.readline
 
 T = int(read())
 result = []
+
 for _ in range(T):
     N = int(read())
-    max, max_s = 0, ''
+    max, school = 0, ''
     for _ in range(N):
         S, L = read().split()
         # 최댓값과 학교 이름을 비교해 갱신
         if int(L) > max:
-            max_s = S
+            school = S
             max = int(L)
-    result.append(max_s)
+    result.append(school)
     
 for r in result: 
     print(r)
--- Baekjoon/정렬/1524 세준세비.py
+++ Baekjoon/정렬/1524 세준세비.py
@@ -8,8 +8,10 @@ for _ in range(T):
     N, M = map(int, read().split())
     sejun = list(map(int, read().split()))
     sebi = list(map(int, read().split()))
+    # 최소 힙으로 정렬
     heapq.heapify(sejun)
     heapq.heapify(sebi)
+    # 둘 다 원소가 있는 경우
     while sejun and sebi:
         if sejun[0] >= sebi[0]:
             heapq.heappop(sebi)
