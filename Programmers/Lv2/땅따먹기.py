8239e4107ad5ba80a7bb2fff7516098124228569
김정우 <jwo0o0358@gmail.com>
2025-04-09 21:05:41 +0900

    [프로그래머스] 땅따먹기

---
+++ Programmers/Lv2/땅따먹기.py
@@ -0,0 +1,7 @@
+def solution(land):
+    N = len(land)
+    for i in range(1, N):
+        for j in range(4):
+            land[i][j] += max(land[i-1][0:j] + land[i-1][j+1:])         
+
+    return max(land[-1])
