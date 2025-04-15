68a82c600bfa6a624a640b9fe5a1e605ef46878c
김정우 <jwo0o0358@gmail.com>
2025-04-15 13:42:39 +0900

    [백준] 11726 2xn 타일링

---
+++ Baekjoon/다이나믹 프로그래밍/11726 2xn 타일링.py
@@ -0,0 +1,12 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+
+memo = [0] * (n+1)
+memo[0] = 1
+memo[1] = 1
+for i in range(2, n+1):
+    memo[i] = memo[i-1] + memo[i-2]
+    
+print(memo[n] % 10007)
